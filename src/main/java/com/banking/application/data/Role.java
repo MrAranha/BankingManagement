1c87d76a5c85ea958447f91b44285660663cffef
MrAranha <aranha.breno53@gmail.com>
2023-09-30 16:31:46 -0300

    Initial commit

---
+++ src/main/java/com/banking/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.banking.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/banking/application/data/entity/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.banking.application.data.entity;
+
+import org.springframework.lang.NonNullApi;
